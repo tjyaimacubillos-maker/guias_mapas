cd8b2b748f282ab5b5dacc15241ba62cebcb49e6
Thania <tjyaimacubillos@gmail.com>
2026-06-09 09:33:08 -0500

    first commit

---
+++ bin/guias_mapas.dart
@@ -0,0 +1,5 @@
+import 'package:guias_mapas/guias_mapas.dart' as guias_mapas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${guias_mapas.calculate()}!');
+}
---
+++ test/guias_mapas_test.dart
@@ -0,0 +1,8 @@
+import 'package:guias_mapas/guias_mapas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
