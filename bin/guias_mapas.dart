b35fbc5c4300970c1fdd63931146e0dc51742899
Thania <tjyaimacubillos@gmail.com>
2026-06-10 06:23:19 -0500

    version 3 arreglada

--- bin/guias_mapas.dart
+++ bin/guias_mapas.dart
@@ -1,10 +1,11 @@
+import 'package:guias_mapas/buscar.dart';
+import 'package:guias_mapas/listar.dart';
+
 void main(List<String> arguments) {
 Map<String,String> agenda = {"Alexander":"30000","Tania":"3000001","Cristiam":"3000054"};
 
-print("${agenda["Alexander"]}");
-print("${agenda["Tania"]}");
-print("${agenda["Cristiam"]}");
+listarcontactos(agenda);
+buscarcontactos(agenda);
 
 }
-
 
